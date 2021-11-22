6dffbefbd6afa8cbd53993a43d9808a058b52b76
Duckfero <loic.matter@viacesi.fr>
2021-11-22 17:29:26 +0100

    Début get et add JSON

---
+++ 1.0/1.0/Path.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Options
+{
+    public class Path
+    {
+        public string Name { get; set; }
+        public string Source { get; set; }
+        public string Destination { get; set; }
+    }
+}
---
+++ 1.0/1.0/Preset.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Options
+{
+    public class Preset
+    {
+        public static void EditPreset()
+        {
+            var path = new Path
+            {
+                Name = "",
+                Source = "",
+                Destination = ""
+            };
+
+            var options = new JsonSerializerOptions { WriteIndented = true };
+            string jsonString = JsonSerializer.Serialize(path, options);
+
+            Console.WriteLine(jsonString);
+        }
+
+        //public static async Task EditPreset()
+        //{
+            //var path = new Path
+            //{
+            //    Name = "",
+            //    Source = "",
+            //    Destination = ""
+            //};
+
+            //string fileName = "WeatherForecast.json";
+            //using FileStream createStream = File.Create(fileName);
+            //await JsonSerializer.SerializeAsync(createStream, path);
+            //await createStream.DisposeAsync();
+
+            //Console.WriteLine(File.ReadAllText(fileName));
+        //}
+        
+    }
+}
