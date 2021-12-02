9e7d19cdb662863b7795df572685de077ee9e516
PrinceLucario <61843570+PrinceLucario@users.noreply.github.com>
2021-12-02 11:21:15 +0100

    Grid 2

---
+++ EasySave 2.0/EasySave 2.0/MV global.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Projet
+{
+    class MV_global
+    {
+        public void ExitApp()
+        {
+            Environment.Exit(621);
+        }
+    }
+}
--- EasySave 2.0/EasySave 2.0/MainWindow.xaml.cs
+++ EasySave 2.0/EasySave 2.0/MainWindow.xaml.cs
@@ -25,55 +25,11 @@ namespace EasySave_2._0
             InitializeComponent();
         }
 
-        class MyCustomPanel : Panel
+        public void ExitApp(object sender, RoutedEventArgs e)
         {
-            bool _wrap = false;
-
-            public MyCustomPanel()
-            {
-            }
-
-            protected override Size MeasureOverride(Size constraint)
-            {
-                if (this.Children.Count < 2)
-                {
-                    return base.MeasureOverride(constraint);
-                }
-                Size finalSize = new Size();
-
-                Children[0].Measure(constraint);
-                Children[1].Measure(constraint);
-
-                if (Children[0].DesiredSize.Width + Children[1].DesiredSize.Width <= constraint.Width)
-                {
-                    _wrap = false;
-                    finalSize.Width = Children[0].DesiredSize.Width + Children[1].DesiredSize.Width;
-                    finalSize.Height = Math.Max(Children[0].DesiredSize.Height, Children[1].DesiredSize.Height);
-                }
-                else
-                {
-                    _wrap = true;
-                    finalSize.Height = Children[0].DesiredSize.Height + Children[1].DesiredSize.Height;
-                    finalSize.Width = Math.Max(Children[0].DesiredSize.Width, Children[1].DesiredSize.Width);
-                }
-                return finalSize;
-            }
-
-            protected override Size ArrangeOverride(Size finalSize)
-            {
-                if (_wrap)
-                {
-                    Children[0].Arrange(new Rect(0, 0, finalSize.Width, finalSize.Height));
-                    Children[1].Arrange(new Rect(0, Children[0].RenderSize.Height, finalSize.Width, finalSize.Height - Children[0].RenderSize.Height));
-                }
-                else
-                {
-                    Children[0].Arrange(new Rect(0, 0, finalSize.Width, finalSize.Height));
-                    Children[1].Arrange(new Rect(finalSize.Width - Children[1].DesiredSize.Width, 0, Children[1].DesiredSize.Width, finalSize.Height));
-
-                }
-                return base.ArrangeOverride(finalSize);
-            }
+            Environment.Exit(621);
         }
+
+
     }
 }
